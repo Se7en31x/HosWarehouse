6df2926c6dbbdb10b5bc6672b55a8928b58e8300
Log <naruephon.wongchai@gmail.com>
2025-04-10 01:39:03 +0700

    approval

--- frontend/src/app/approvalRequest/page.js
+++ frontend/src/app/approvalRequest/page.js
@@ -1,5 +1,25 @@
+"use client";
+
+import styles from "./page.module.css";
+
 export default function approvalRequest(){
     return(
-        <h1>ตรวจสอบคำขอเบิก</h1>
+        <div className={styles.mainHome}>
+
+        <div className={styles.dashboardContainer}>
+        {/* กราฟข้อมูลการทำรายการ */}
+            <div className={styles.chartdata}>
+                <div className={styles.cardHeader}>ข้อมูลการทำรายการ</div>
+                <div className={styles.chartPlaceholder}>{/* ใส่กราฟที่นี่ */}</div>
+            </div> 
+            
+            {/* กล่องสถานะคำขอ + กิจกรรมล่าสุด */}
+        <div className={styles.infoContainer}>
+          </div>
+
+          <div className={styles.recentActivity}>
+          </div>
+        </div>
+    </div>
     );
 }
