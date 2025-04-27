3dc9467590e8c045a5b4be937843d53434c6c9f9
65021969 <peerapat3250@gmail.com>
2025-04-28 02:41:22 +0700

    update inventory

--- frontend/src/app/inventoryCheck/page.js
+++ frontend/src/app/inventoryCheck/page.js
@@ -1,13 +1,17 @@
 "use client";
 import { useState } from "react";
 import styles from "./page.module.css";
+import axiosInstance from '../utils/axiosInstance';
 
 export default function inventoryCheck() {
-  const [filter, setFilter] = useState("");
-  const [category, setCategory] = useState("");
-  const [unit, setUnit] = useState("");
-  const [storage, setStorage] = useState("");
 
+  const [filter, setFilter] = useState();
+  const [category, setCategory] = useState();
+  const [unit, setUnit] = useState();
+  const [storage, setStorage] = useState();
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 9; // จำนวนรายการที่แสดงต่อหน้า
   const handleFilterChange = (event) => {
     setFilter(event.target.value);
   };
@@ -30,7 +34,7 @@ export default function inventoryCheck() {
       id: 1,
       image: "https://example.com/bandage.jpg", // ลิงก์ของรูปภาพ
       category: "ผ้าพันแผล",
-      type: "เวชภัณฑ์",
+      type: "อุปกรณ์ทางการแพทย์",
       quantity: 100,
       unit: "กล่อง",
       status: "พร้อมใช้งาน",
@@ -43,6 +47,118 @@ export default function inventoryCheck() {
       id: 2,
       image: "https://example.com/medical_supplies.jpg", // ตัวอย่างอีกภาพ
       category: "เวชภัณฑ์",
+      type: "อุปกรณ์ทางการแพทย์",
+      quantity: 50,
+      unit: "ชุด",
+      status: "พร้อมใช้งาน",
+      storage: "คลังกลาง",
+      lastUpdated: "2025-12-25",
+      action: "ตรวจสอบ",
+    },
+    {
+      id: 3,
+      image: "https://example.com/bandage.jpg", // ลิงก์ของรูปภาพ
+      category: "ผ้าพันแผล",
+      type: "อุปกรณ์ทางการแพทย์",
+      quantity: 100,
+      unit: "กล่อง",
+      status: "พร้อมใช้งาน",
+      storage: "คลังกลาง",
+      lastUpdated: "2025-12-31",
+      action: "ตรวจสอบ",
+    },
+    // คุณสามารถเพิ่มข้อมูลตัวอย่างเพิ่มได้ที่นี่
+    {
+      id: 4,
+      image: "https://example.com/medical_supplies.jpg", // ตัวอย่างอีกภาพ
+      category: "เวชภัณฑ์",
+      type: "อุปกรณ์ทางการแพทย์",
+      quantity: 50,
+      unit: "ชุด",
+      status: "พร้อมใช้งาน",
+      storage: "คลังกลาง",
+      lastUpdated: "2025-12-25",
+      action: "ตรวจสอบ",
+    },
+    {
+      id: 5,
+      image: "https://example.com/bandage.jpg", // ลิงก์ของรูปภาพ
+      category: "ผ้าพันแผล",
+      type: "อุปกรณ์ทางการแพทย์",
+      quantity: 100,
+      unit: "กล่อง",
+      status: "พร้อมใช้งาน",
+      storage: "คลังกลาง",
+      lastUpdated: "2025-12-31",
+      action: "ตรวจสอบ",
+    },
+    // คุณสามารถเพิ่มข้อมูลตัวอย่างเพิ่มได้ที่นี่
+    {
+      id: 6,
+      image: "https://example.com/medical_supplies.jpg", // ตัวอย่างอีกภาพ
+      category: "เวชภัณฑ์",
+      type: "อุปกรณ์ทางการแพทย์",
+      quantity: 50,
+      unit: "ชุด",
+      status: "พร้อมใช้งาน",
+      storage: "คลังกลาง",
+      lastUpdated: "2025-12-25",
+      action: "ตรวจสอบ",
+    },
+    {
+      id: 7,
+      image: "https://example.com/medical_supplies.jpg", // ตัวอย่างอีกภาพ
+      category: "เวชภัณฑ์",
+      type: "อุปกรณ์ทางการแพทย์",
+      quantity: 50,
+      unit: "ชุด",
+      status: "พร้อมใช้งาน",
+      storage: "คลังกลาง",
+      lastUpdated: "2025-12-25",
+      action: "ตรวจสอบ",
+    },
+
+    {
+      id: 8,
+      image: "https://example.com/medical_supplies.jpg", // ตัวอย่างอีกภาพ
+      category: "เวชภัณฑ์",
+      type: "อุปกรณ์ทางการแพทย์",
+      quantity: 50,
+      unit: "ชุด",
+      status: "พร้อมใช้งาน",
+      storage: "คลังกลาง",
+      lastUpdated: "2025-12-25",
+      action: "ตรวจสอบ",
+    },
+    {
+      id: 9,
+      image: "https://example.com/medical_supplies.jpg", // ตัวอย่างอีกภาพ
+      category: "เวชภัณฑ์",
+      type: "อุปกรณ์ทางการแพทย์",
+      quantity: 50,
+      unit: "ชุด",
+      status: "พร้อมใช้งาน",
+      storage: "คลังกลาง",
+      lastUpdated: "2025-12-25",
+      action: "ตรวจสอบ",
+    },
+    {
+      id: 10,
+      image: "https://example.com/medical_supplies.jpg", // ตัวอย่างอีกภาพ
+      category: "เวชภัณฑ์",
+      type: "อุปกรณ์ทางการแพทย์",
+      quantity: 50,
+      unit: "ชุด",
+      status: "พร้อมใช้งาน",
+      storage: "คลังกลาง",
+      lastUpdated: "2025-12-25",
+      action: "ตรวจสอบ",
+    },
+    {
+      id: 11,
+      image: "https://example.com/medical_supplies.jpg", // ตัวอย่างอีกภาพ
+      category: "เวชภัณฑ์",
+      type: "อุปกรณ์ทางการแพทย์",
       quantity: 50,
       unit: "ชุด",
       status: "พร้อมใช้งาน",
@@ -52,30 +168,44 @@ export default function inventoryCheck() {
     },
   ];
 
+  const currentItems = inventoryData.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+  
+  const handleNextPage = () => {
+    if (currentPage * itemsPerPage < inventoryData.length) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
   return (
     <div className={styles.mainHome}>
       {/* แถบบน */}
-        <div className={styles.bar}>
-          <ul className={styles.navList}>
-            <li className={styles.navItem}>ตรวจสอบยอดคงคลัง</li>
-            <li className={styles.navItem}>ยา</li>
-            <li className={styles.navItem}>เวชภัณฑ์</li>
-            <li className={styles.navItem}>ครุภัณฑ์</li>
-            <li className={styles.navItem}>อุปกรณ์ทางการแพทย์</li>
-            <li className={styles.navItem}>ของใช้ทั่วไป</li>
-          </ul>
-        </div>
+      <div className={styles.bar}>
+        <ul className={styles.navList}>
+          <li className={styles.navItem}>ตรวจสอบยอดคงคลัง</li>
+          <li className={styles.navItem}>ยา</li>
+          <li className={styles.navItem}>เวชภัณฑ์</li>
+          <li className={styles.navItem}>ครุภัณฑ์</li>
+          <li className={styles.navItem}>อุปกรณ์ทางการแพทย์</li>
+          <li className={styles.navItem}>ของใช้ทั่วไป</li>
+        </ul>
+      </div>
 
-         {/* ส่วนของ infoContainer */}
+      {/* ส่วนของ infoContainer */}
       <div className={styles.infoContainer}>
         {/* ตัวกรอง */}
         <div className={styles.filterContainer}>
           <div className={styles.filterGroup}>
             <label htmlFor="category" className={styles.filterLabel}>หมวดหมู่:</label>
-            <select 
-              id="category" 
-              className={styles.filterSelect} 
-              value={category} 
+            <select
+              id="category"
+              className={styles.filterSelect}
+              value={category}
               onChange={handleCategoryChange}>
               <option value="">เลือกหมวดหมู่</option>
               <option value="ยา">ยา</option>
@@ -88,10 +218,10 @@ export default function inventoryCheck() {
 
           <div className={styles.filterGroup}>
             <label htmlFor="unit" className={styles.filterLabel}>หน่วย:</label>
-            <select 
-              id="unit" 
-              className={styles.filterSelect} 
-              value={unit} 
+            <select
+              id="unit"
+              className={styles.filterSelect}
+              value={unit}
               onChange={handleUnitChange}>
               <option value="">เลือกหน่วย</option>
               <option value="ขวด">ขวด</option>
@@ -103,10 +233,10 @@ export default function inventoryCheck() {
 
           <div className={styles.filterGroup}>
             <label htmlFor="storage" className={styles.filterLabel}>สถานที่จัดเก็บ:</label>
-            <select 
-              id="storage" 
-              className={styles.filterSelect} 
-              value={storage} 
+            <select
+              id="storage"
+              className={styles.filterSelect}
+              value={storage}
               onChange={handleStorageChange}>
               <option value="">เลือกสถานที่จัดเก็บ</option>
               <option value="ห้องเก็บยา">ห้องเก็บยา</option>
@@ -118,50 +248,74 @@ export default function inventoryCheck() {
           {/* ช่องค้นหา */}
           <div className={styles.filterGroupSearch}>
             <label htmlFor="filter" className={styles.filterLabel}>ค้นหาข้อมูล:</label>
-            <input 
-              type="text" 
-              id="filter" 
-              className={styles.filterInput} 
-              value={filter} 
-              onChange={handleFilterChange} 
-              placeholder="กรอกเพื่อค้นหา..." 
+            <input
+              type="text"
+              id="filter"
+              className={styles.filterInput}
+              value={filter}
+              onChange={handleFilterChange}
+              placeholder="กรอกเพื่อค้นหา..."
             />
           </div>
         </div>
         {/* แถบหัวข้อคล้าย Excel */}
-      <div className={styles.tableHeader}>
-        <div className={styles.headerItem}>หมายเลข</div>
-        <div className={styles.headerItem}>รูปภาพ</div>
-        <div className={styles.headerItem}>ชื่อ</div>
-        <div className={styles.headerItem}>หมวดหมู่</div>
-        <div className={styles.headerItem}>จำนวน</div>
-        <div className={styles.headerItem}>หน่วย</div>
-        <div className={styles.headerItem}>สถานะ</div>
-        <div className={styles.headerItem}>สถานที่จัดเก็บ</div>
-        <div className={styles.headerItem}>วันที่อัปเดตล่าสุด</div>
-        <div className={styles.headerItem}>ดำเนินการ</div>
-      </div>
-      {/* แสดงข้อมูลในตาราง */}
-      <div className={styles.inventory}>
-        {inventoryData.map((item) => (
-          <div className={styles.tableRow} key={item.id}>
-            <div className={styles.tableCell}>{item.id}</div>
-            <div className={styles.tableCell}>
-              <img src={item.image} alt={item.category} className={styles.imageCell} />
-            </div>
-            <div className={styles.tableCell}>{item.category}</div>
-            <div className={styles.tableCell}>{item.type}</div>
-            <div className={styles.tableCell}>{item.quantity}</div>
-            <div className={styles.tableCell}>{item.unit}</div>
-            <div className={styles.tableCell}>{item.status}</div>
-            <div className={styles.tableCell}>{item.storage}</div>
-            <div className={styles.tableCell}>{item.lastUpdated}</div>
-            <div className={styles.tableCell}>
-              <button className={styles.actionButton}>{item.action}</button>
-            </div>
-          </div>
-        ))}
+        <div className={`${styles.tableGrid} ${styles.tableHeader}`}>
+          <div className={styles.headerItem}>ลำดับ</div>
+          <div className={styles.headerItem}>รูปภาพ</div>
+          <div className={styles.headerItem}>รายการ</div>
+          <div className={styles.headerItem}>หมวดหมู่</div>
+          <div className={styles.headerItem}>คงเหลือ</div>
+          <div className={styles.headerItem}>หน่วย</div>
+          <div className={styles.headerItem}>สถานะ</div>
+          <div className={styles.headerItem}>สถานที่จัดเก็บ</div>
+          <div className={styles.headerItem}>อัปเดตล่าสุด</div>
+          <div className={styles.headerItem}>ดำเนินการ</div>
         </div>
+        {/* แสดงข้อมูลในตาราง */}
+        <div className={styles.inventory}>
+          {currentItems.map((item) => (
+            <div className={`${styles.tableGrid} ${styles.tableRow}`} key={item.id}>
+              <div className={styles.tableCell}>{item.id}</div>
+              <div className={styles.tableCell}>
+                <img
+                  src="https://medthai.com/wp-content/uploads/2016/11/%E0%B8%8B%E0%B8%B5%E0%B8%A1%E0%B8%AD%E0%B8%A5.jpg"
+                  alt={item.category}
+                  className={styles.imageCell}
+                  style={{ width: '70px', height: '70px', objectFit: 'cover' }} // กำหนดขนาดที่นี่
+                />
+              </div>
+              <div className={styles.tableCell}>{item.category}</div>
+              <div className={styles.tableCell}>{item.type}</div>
+              <div className={styles.tableCell}>{item.quantity}</div>
+              <div className={styles.tableCell}>{item.unit}</div>
+              <div className={styles.tableCell}>{item.status}</div>
+              <div className={styles.tableCell}>{item.storage}</div>
+              <div className={styles.tableCell}>{item.lastUpdated}</div>
+              <div className={styles.tableCell}>
+                <button className={styles.actionButton}>{item.action}</button>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className={styles.pagination}>
+          {/* ปุ่มย้อนกลับ */}
+          <button
+            className={styles.prevButton}
+            onClick={handlePrevPage}
+            disabled={currentPage === 1}>
+            หน้าก่อนหน้า
+          </button>
+
+          {/* ปุ่มหน้าถัดไป */}
+          <button
+            className={styles.nextButton}
+            onClick={handleNextPage}
+            disabled={currentPage * itemsPerPage >= inventoryData.length}>
+            หน้าถัดไป
+          </button>
+        </div>
+
       </div>
 
 
