04fcb4273f386e904856d0c7cc0dfb076a847c23
Log <naruephon.wongchai@gmail.com>
2025-04-28 19:10:12 +0700

    update manageData

--- frontend/src/app/manageData/page.js
+++ frontend/src/app/manageData/page.js
@@ -5,7 +5,8 @@ import { useRouter } from "next/navigation";
 import styles from "./page.module.css";
 
 
-export default function manageData(){
+export default function ManageDataPage() {
+
     const router = useRouter();
 
     const [filter, setFilter] = useState("");
@@ -13,6 +14,9 @@ export default function manageData(){
     const [unit, setUnit] = useState("");
     const [storage, setStorage] = useState("");
   
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 9; // จำนวนรายการที่แสดงต่อหน้า
+
     const handleFilterChange = (event) => {
       setFilter(event.target.value);
     };
@@ -44,7 +48,190 @@ export default function manageData(){
       action: "แก้ไข",
       action2: "ลบ",
     },
+    {
+      id: "12345",
+      image: "", // ลิงก์ของรูปภาพ
+      name: "ผ้าผันแผล",
+      type: "เวชภัณฑ์",
+      quantity: 100,
+      unit: "กล่อง",
+      status: "พร้อมใช้งาน",
+      location: "คลังกลาง",
+      dateexd: "30-02-2025",
+      edited: "30-02-2025",
+      action: "แก้ไข",
+      action2: "ลบ",
+    },
+    {
+      id: "12345",
+      image: "", // ลิงก์ของรูปภาพ
+      name: "ผ้าผันแผล",
+      type: "เวชภัณฑ์",
+      quantity: 100,
+      unit: "กล่อง",
+      status: "พร้อมใช้งาน",
+      location: "คลังกลาง",
+      dateexd: "30-02-2025",
+      edited: "30-02-2025",
+      action: "แก้ไข",
+      action2: "ลบ",
+    },
+    {
+      id: "12345",
+      image: "", // ลิงก์ของรูปภาพ
+      name: "ผ้าผันแผล",
+      type: "เวชภัณฑ์",
+      quantity: 100,
+      unit: "กล่อง",
+      status: "พร้อมใช้งาน",
+      location: "คลังกลาง",
+      dateexd: "30-02-2025",
+      edited: "30-02-2025",
+      action: "แก้ไข",
+      action2: "ลบ",
+    },
+    {
+      id: "12345",
+      image: "", // ลิงก์ของรูปภาพ
+      name: "ผ้าผันแผล",
+      type: "เวชภัณฑ์",
+      quantity: 100,
+      unit: "กล่อง",
+      status: "พร้อมใช้งาน",
+      location: "คลังกลาง",
+      dateexd: "30-02-2025",
+      edited: "30-02-2025",
+      action: "แก้ไข",
+      action2: "ลบ",
+    },
+    {
+      id: "12345",
+      image: "", // ลิงก์ของรูปภาพ
+      name: "ผ้าผันแผล",
+      type: "เวชภัณฑ์",
+      quantity: 100,
+      unit: "กล่อง",
+      status: "พร้อมใช้งาน",
+      location: "คลังกลาง",
+      dateexd: "30-02-2025",
+      edited: "30-02-2025",
+      action: "แก้ไข",
+      action2: "ลบ",
+    },
+    {
+      id: "12345",
+      image: "", // ลิงก์ของรูปภาพ
+      name: "ผ้าผันแผล",
+      type: "เวชภัณฑ์",
+      quantity: 100,
+      unit: "กล่อง",
+      status: "พร้อมใช้งาน",
+      location: "คลังกลาง",
+      dateexd: "30-02-2025",
+      edited: "30-02-2025",
+      action: "แก้ไข",
+      action2: "ลบ",
+    },
+    {
+      id: "12345",
+      image: "", // ลิงก์ของรูปภาพ
+      name: "ผ้าผันแผล",
+      type: "เวชภัณฑ์",
+      quantity: 100,
+      unit: "กล่อง",
+      status: "พร้อมใช้งาน",
+      location: "คลังกลาง",
+      dateexd: "30-02-2025",
+      edited: "30-02-2025",
+      action: "แก้ไข",
+      action2: "ลบ",
+    },
+    {
+      id: "12345",
+      image: "", // ลิงก์ของรูปภาพ
+      name: "ผ้าผันแผล",
+      type: "เวชภัณฑ์",
+      quantity: 100,
+      unit: "กล่อง",
+      status: "พร้อมใช้งาน",
+      location: "คลังกลาง",
+      dateexd: "30-02-2025",
+      edited: "30-02-2025",
+      action: "แก้ไข",
+      action2: "ลบ",
+    },
+    {
+      id: "12345",
+      image: "", // ลิงก์ของรูปภาพ
+      name: "ผ้าผันแผล",
+      type: "เวชภัณฑ์",
+      quantity: 100,
+      unit: "กล่อง",
+      status: "พร้อมใช้งาน",
+      location: "คลังกลาง",
+      dateexd: "30-02-2025",
+      edited: "30-02-2025",
+      action: "แก้ไข",
+      action2: "ลบ",
+    },
+    {
+      id: "12345",
+      image: "", // ลิงก์ของรูปภาพ
+      name: "ผ้าผันแผล",
+      type: "เวชภัณฑ์",
+      quantity: 100,
+      unit: "กล่อง",
+      status: "พร้อมใช้งาน",
+      location: "คลังกลาง",
+      dateexd: "30-02-2025",
+      edited: "30-02-2025",
+      action: "แก้ไข",
+      action2: "ลบ",
+    },
+    {
+      id: "12345",
+      image: "", // ลิงก์ของรูปภาพ
+      name: "ผ้าผันแผล",
+      type: "เวชภัณฑ์",
+      quantity: 100,
+      unit: "กล่อง",
+      status: "พร้อมใช้งาน",
+      location: "คลังกลาง",
+      dateexd: "30-02-2025",
+      edited: "30-02-2025",
+      action: "แก้ไข",
+      action2: "ลบ",
+    },
+    {
+      id: "12345",
+      image: "", // ลิงก์ของรูปภาพ
+      name: "ผ้าผันแผล",
+      type: "เวชภัณฑ์",
+      quantity: 100,
+      unit: "กล่อง",
+      status: "พร้อมใช้งาน",
+      location: "คลังกลาง",
+      dateexd: "30-02-2025",
+      edited: "30-02-2025",
+      action: "แก้ไข",
+      action2: "ลบ",
+    },
     ]
+
+    const currentItems = manageData.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
+
+    const handlePrevPage = () => {
+        if (currentPage > 1) {
+        setCurrentPage(currentPage - 1);
+        }
+    };
+    
+    const handleNextPage = () => {
+        if (currentPage * itemsPerPage < manageData.length) {
+        setCurrentPage(currentPage + 1);
+        }
+    };
+
     return(
         <div className={styles.mainHome}>
             <div className={styles.infoContainer}>
@@ -120,7 +307,7 @@ export default function manageData(){
                 </div>
 
                 {/* แถบหัวข้อคล้าย Excel */}
-                <div className={styles.tableHeader}>
+                <div className={`${styles.tableGrid} ${styles.tableHeader}`}>
                     <div className={styles.headerItem}>หมายเลข</div>
                     <div className={styles.headerItem}>รูปภาพ</div>
                     <div className={styles.headerItem}>ชื่อ</div>
@@ -129,32 +316,55 @@ export default function manageData(){
                     <div className={styles.headerItem}>หน่วย</div>
                     <div className={styles.headerItem}>สถานะ</div>
                     <div className={styles.headerItem}>สถานที่จัดเก็บ</div>
-                    <div className={styles.headerItem}>วันหมดอายุ</div>
                     <div className={styles.headerItem}>แก้ไขล่าสุด</div>
                     <div className={styles.headerItem}>การจัดการ</div>
                 </div>
               
                 {/* แสดงข้อมูลในตาราง */}
                 <div className={styles.inventory}>
-                    {manageData.map((item) => (
-                    <div className={styles.tableRow} key={item.id}>
-                        <div className={styles.tableCell}>{item.id}</div>
-                        <div className={styles.tableCell}>{item.image}</div>
-                        <div className={styles.tableCell}>{item.name}</div>
-                        <div className={styles.tableCell}>{item.type}</div>
-                        <div className={styles.tableCell}>{item.quantity}</div>
-                        <div className={styles.tableCell}>{item.unit}</div>
-                        <div className={styles.tableCell}>{item.status}</div>
-                        <div className={styles.tableCell}>{item.location}</div>
-                        <div className={styles.tableCell}>{item.dateexd}</div>
-                        <div className={styles.tableCell}>{item.edited}</div>
-                        <div className={styles.tableCell}>
-                            <button className={`${styles.actionButton} ${styles.editButton}`}>{item.action}</button>
-                            <button className={`${styles.actionButton} ${styles.deleteButton}`}>{item.action2}</button>
-                        </div>
+                {currentItems.map((item) => (
+                    <div className={`${styles.tableGrid} ${styles.tableRow}`} key={item.id}>
+                    <div className={styles.tableCell}>{item.id}</div>
+                    <div className={`${styles.tableCell} ${styles.centerCell}`}>
+                        <img src="https://medthai.com/wp-content/uploads/2016/11/%E0%B8%8B%E0%B8%B5%E0%B8%A1%E0%B8%AD%E0%B8%A5.jpg"
+                        alt={item.category}
+                        className={styles.imageCell}
+                        style={{ width: '70px', height: '70px', objectFit: 'cover' }} // กำหนดขนาดที่นี่} 
+                        />
                     </div>
-                    ))}
+                    <div className={styles.tableCell}>{item.name}</div>
+                    <div className={styles.tableCell}>{item.type}</div>
+                    <div className={styles.tableCell}>{item.quantity}</div>
+                    <div className={styles.tableCell}>{item.unit}</div>
+                    <div className={styles.tableCell}>{item.status}</div>
+                    <div className={styles.tableCell}>{item.location}</div>
+                    <div className={styles.tableCell}>{item.edited}</div>
+                    <div className={`${styles.tableCell} ${styles.centerCell}`}>
+                        <button className={`${styles.actionButton} ${styles.editButton}`}>{item.action}</button>
+                        <button className={`${styles.actionButton} ${styles.deleteButton}`}>{item.action2}</button>
                     </div>
+                    </div>
+                ))}
+                </div>
+
+                    <div className={styles.pagination}>
+                    {/* ปุ่มย้อนกลับ */}
+                        <button
+                            className={styles.prevButton}
+                            onClick={handlePrevPage}
+                            disabled={currentPage === 1}>
+                            หน้าก่อนหน้า
+                        </button>
+
+                    {/* ปุ่มหน้าถัดไป */}
+                        <button
+                            className={styles.nextButton}
+                            onClick={handleNextPage}
+                            disabled={currentPage * itemsPerPage >= manageData.length}>
+                            หน้าถัดไป
+                        </button>
+                    </div>
+
                 </div>
             </div>
     );
