e0ab90eff16463e6ec1ad5729b5d4741bbaa3805
Log <naruephon.wongchai@gmail.com>
2025-05-03 18:00:51 +0700

    inventoryCheck

--- frontend/src/app/inventoryCheck/page.js
+++ frontend/src/app/inventoryCheck/page.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import axiosInstance from '../utils/axiosInstance';
 import Link from "next/link";
-import { connectSocket, disconnectSocket, sendMessage, joinRoom } from '../utils/socket';
+// import { connectSocket, disconnectSocket, sendMessage, joinRoom } from '../utils/socket';
 export default function InventoryCheck() {
 
   const [filter, setFilter] = useState();
@@ -65,20 +65,20 @@ export default function InventoryCheck() {
 
 
   ///////////////////// API///////////////////////////
-  useEffect(() => {
-    const socket = connectSocket(); // ได้ socket กลับมา
+  // useEffect(() => {
+  //   const socket = connectSocket(); // ได้ socket กลับมา
 
-    socket.emit('requestInventoryData'); // 👈 ขอข้อมูลจาก backend
+  //   socket.emit('requestInventoryData'); // 👈 ขอข้อมูลจาก backend
 
-    socket.on('itemsData', (items) => {
-      console.log('📦 Received inventory:', items);
-      setInventoryData(items); // แสดงผลใน UI
-    });
+  //   socket.on('itemsData', (items) => {
+  //     console.log('📦 Received inventory:', items);
+  //     setInventoryData(items); // แสดงผลใน UI
+  //   });
 
-    return () => {
-      disconnectSocket();
-    };
-  }, []);
+  //   return () => {
+  //     disconnectSocket();
+  //   };
+  // }, []);
 
   console.log(inventoryData.map(item => item.id));
   return (
