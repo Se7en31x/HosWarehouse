6528c7ab1595751ec563168444c7a59ba54257fc
Log <naruephon.wongchai@gmail.com>
2025-08-03 04:22:28 +0700

    update staff

--- frontend/src/app/staff/cart/page.jsx
+++ frontend/src/app/staff/cart/page.jsx
@@ -211,7 +211,7 @@ export default function Cart() {
   return (
     <div className={styles.container}>
       <h2 className={styles.header}>
-        รายการ{translateAction(cartItems[0]?.action || '')}
+        รายการร{translateAction(cartItems[0]?.action || '')}
       </h2>
       <div className={styles.tableContainer}>
         <table className={styles.table}>
