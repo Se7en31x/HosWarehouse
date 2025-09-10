81a93225f05a2c6628cd1a8033c7170bea5e770e
65021969 <peerapat3250@gmail.com>
2025-09-10 16:48:57 +0700

    update f

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -5,7 +5,12 @@ const nextConfig = {
       'localhost',
       'ijvzxcyfkvahhlveukow.supabase.co'
     ],
-    
+  },
+  // ✅ เพิ่มการตั้งค่านี้เข้าไป
+  experimental: {
+    // This option allows global selectors like `:root` in CSS Modules files.
+    // Use with caution, as it can lead to style conflicts.
+    allowGlobal: true,
   },
 };
 
